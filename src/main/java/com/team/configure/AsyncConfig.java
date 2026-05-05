4677b906d614e57da0f7205630a9aec8173c42a4
jc26586499 <joycejava26@gmail.com>
2026-05-05 18:23:09 +0800

    @enable // for 排程寄信

--- src/main/java/com/team/configure/AsyncConfig.java
+++ src/main/java/com/team/configure/AsyncConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-@EnableAsync
+@EnableAsync //新增for排程寄信
 public class AsyncConfig {
     // 啟用 @Async，EmailService.sendContactReplyNotification 會在背景執行緒寄信
     // 不阻塞 API 回應
